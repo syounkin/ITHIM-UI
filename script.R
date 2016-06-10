41113bf1d0d4a8691d530bdd7a8e01c87d477608
Samuel Younkin <samuel.younkin@gmail.com>
2016-06-10 10:00:44 -0500

    Updated .gitgnore and script.R

--- script.R
+++ script.R
@@ -1,3 +1,4 @@
 library("shiny")
-runGitHub( "ITHIM-UI", "syounkin", ref = "devel")
+#runGitHub( "ITHIM-UI", "syounkin", ref = "devel")
+runGitHub( "ITHIM-UI", "syounkin", ref = "master")
 runApp("~/ITHIM-UI/")
