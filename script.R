1e022cdbea0cd0b40369d8c8e02c25dcb554282b
Samuel Younkin <samuel.younkin@gmail.com>
2016-06-16 18:02:50 -0500

    Updated script.R

--- script.R
+++ script.R
@@ -1,12 +1,12 @@
-library("shiny")
-library("devtools")
+#library("shiny");library("devtools");
 #install_github("syounkin/ITHIM")
-install_github("syounkin/ITHIM", ref = "devel")
-library("ITHIM")
+#install_github("syounkin/ITHIM", ref = "devel")
+#library("ITHIM")
 #runGitHub( "ITHIM-UI", "syounkin")
 runGitHub( "ITHIM-UI", "syounkin", ref = "devel")
 
 
-#library("devtools")
-#install("~/ITHIM/")
-#runApp("~/ITHIM-UI/")
+
+library("shiny");library("devtools");
+install("~/ITHIM/");library("ITHIM");
+runApp("~/ITHIM-UI/")
